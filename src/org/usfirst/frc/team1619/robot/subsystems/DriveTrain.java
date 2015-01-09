27b548aaf15cacb288d120615605db950efcb1b7
Jess <jess@Embers-Computer-9.local>
2015-01-08 19:22:35 -0700

    initial commit

---
+++ src/org/usfirst/frc/team1619/robot/subsystems/DriveTrain.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1619.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    Talon leftMotor;
+    Talon rightMotor;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
